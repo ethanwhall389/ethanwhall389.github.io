9d2e25cc6e1e75a080f6ce06c76cbe1258e94bde
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-02-14 22:56:00 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/ravi-pinisetti-1zikZJVXSfA-unsplash(small).jpg") {
+    myImage.setAttribute("src", "images/ocean.jpg");
+  } else {
+    myImage.setAttribute("src", "images/ravi-pinisetti-1zikZJVXSfA-unsplash(small).jpg")
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool,
+  ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+}
